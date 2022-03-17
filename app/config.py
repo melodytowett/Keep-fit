f5ecc3f364d88cea9e266d22ffe42cdd4d722b0a
melodytowett <melodytowett99@gmail.com>
2022-03-17 23:43:59 +0300

    databse

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = '@Key1621'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://kiama:kiamapwd@localhost/FireFitness'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://moringa:1234@localhost/fitness'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
