443ec74f51605baecf68f78317c3c9cfe479c8b7
davidkiama <davidkiama98@gmail.com>
2022-03-18 11:53:03 +0300

    Live demonstration

--- frontend/src/app/login/trainee-login/trainee-login.component.ts
+++ frontend/src/app/login/trainee-login/trainee-login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-trainee-login',
@@ -9,12 +13,44 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TraineeLoginComponent implements OnInit {
 
+  username: string = '';
+  password: string = '';
+  url: string = environment.API_URL + 'trainee/login';
+
   constructor(private router: Router, private readonly http: HttpClient) { }
   gotoTraineeLogin() {
     this.router.navigate(['trainee-login'])
   }
 
   ngOnInit(): void {
+  }
+
+  loginTrainee() {
+
+    const body = {
+      username: this.username,
+      password: this.password
+    }
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })};
+
+    let response:any={}
+
+
+    this.http.post(this.url, body, httpOptions).subscribe(data=>{
+      response = data
+      if (response.status == 'success') {
+        this.router.navigate(['/trainee']);
+        
+      }else{
+        alert('Login Failed')
+      }
+    })
+
+
   }
 
 }
--- frontend/src/app/login/trainer-login/trainer-login.component.ts
+++ frontend/src/app/login/trainer-login/trainer-login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+
+
 
 @Component({
   selector: 'app-trainer-login',
@@ -9,12 +14,44 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TrainerLoginComponent implements OnInit {
 
+  username: string = '';
+  password: string = '';
+  url: string = environment.API_URL + 'trainer/login';
+
   constructor(private router: Router, private readonly http: HttpClient) { }
   gotoTrainerLogin() {
     this.router.navigate(['trainer-login'])
   }
 
   ngOnInit(): void {
+  }
+
+  loginTrainer() {
+
+    const body = {
+      username: this.username,
+      password: this.password
+    }
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })};
+
+    let response:any={}
+
+
+
+    this.http.post(this.url, body, httpOptions).subscribe(data=>{
+      response = data
+      if (response.status == 'success') {
+        localStorage.setItem('trainerId', response.user_id);
+        this.router.navigate(['/trainer']);
+
+      }else{
+        alert('Login Failed' + response.message)
+      }
+    })
   }
 
 }
